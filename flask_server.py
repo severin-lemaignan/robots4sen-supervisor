5a7e63fafc198c7e6b50e6ec6ee7e0fb9e1b82e4
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2021-04-19 14:49:12 +0100

    Fix Flask crash when the server is run directly

--- flask_server.py
+++ flask_server.py
@@ -7,14 +7,15 @@ from constants import *
 
 tablet_webserver = Flask(__name__)
 
-
+tablet_webserver.cmd_queue = None
 
 
 @tablet_webserver.route('/')
 def main():
 
-    # server.cmd_queue is injected by main.py upon Flask's thread creation
-    tablet_webserver.cmd_queue.put((TABLET, NONE, None))
+    if tablet_webserver.cmd_queue:
+        # server.cmd_queue is injected by main.py upon Flask's thread creation
+        tablet_webserver.cmd_queue.put((TABLET, NONE, None))
 
     return render_template('index.html')
 
