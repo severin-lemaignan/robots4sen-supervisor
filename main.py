0cd538fd5828f1e43e310a2713f467168bd7992f
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2021-04-27 10:47:45 +0100

    logging: better default configuration

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # utf-8
 
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
 logger = logging.getLogger("robots.main")
 
 import argparse
