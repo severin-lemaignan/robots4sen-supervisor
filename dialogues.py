1a5506c3fa77b080a92ff5b71ee656afa09f19fd
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2021-05-10 13:14:05 +0100

    dialogues: minor changes to mood prompts

--- dialogues.py
+++ dialogues.py
@@ -5,12 +5,17 @@ DIALOGUES = {
                     "You want to listen to a story? Great!",
                     "Let see what story we can come up with...",
                     "Ok! Let me tell you a story."],
+
     "story_start": ["Alright, let start the story.", 
                     "Good, good! Let's start the story"],
+
     "story_end": ["The story is finished! I hope you liked it.",
                   "The end. Did you like the story?"],
-    "mood_prompt": ["How do you feel?",
-                   "How are you?"],
+
+    "mood_prompt": ["Hey! Good to see you! How do you feel?",
+                   "Hi! How are you?",
+                   "Nice to see you! How are you?"],
+
     "mood_prompt_activities": ["What do we do?",
                    "Ok! What would you like to do?"]
     }
