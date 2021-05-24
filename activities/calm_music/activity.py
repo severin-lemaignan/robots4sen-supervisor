ff074516f40f9e8fc35a2a5d59e6986149405b32
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2021-05-24 11:26:37 +0100

    activity: calm_music: rename variable to make more sense

--- activities/calm_music/activity.py
+++ activities/calm_music/activity.py
@@ -26,7 +26,7 @@ class CalmMusicActivity:
 
         self.robot.tablet.debug("activity/calm_music")
 
-        self.stop_dance = False
+        self.stop_behaviour = False
 
         # self._behaviour is a generator returning the current activity status;
         # self.tick() (called by the supervisor) will progress through it
@@ -38,14 +38,14 @@ class CalmMusicActivity:
         self.robot.say(get_dialogue("calm_music_start")).wait()
         yield RUNNING
 
-        dances = ["robots4sen-brl/calm_music"]
+        behaviours = ["robots4sen-brl/calm_music"]
 
-        dance = self.robot.run_behaviour(random.choice(dances))
+        behaviour = self.robot.run_behaviour(random.choice(behaviours))
 
-        while dance.isRunning():
-            if self.stop_dance:
-                dance.cancel()
-                self.stop_dance = False
+        while behaviour.isRunning():
+            if self.stop_behaviour:
+                behaviour.cancel()
+                self.stop_behaviour = False
             yield RUNNING
 
     def tick(self, evt=None):
@@ -53,11 +53,11 @@ class CalmMusicActivity:
         if evt:
             if evt.type == ActivityEvent.INTERRUPTED:
                 logger.warning("Activity 'calm music' stopped: interrupt request!");
-                self.stop_dance = True
+                self.stop_behaviour = True
                 return STOPPED
             if evt.type == ActivityEvent.NO_ONE_ENGAGED:
                 logger.warning("Activity 'calm music' stopped: no one in front of the robot!");
-                self.stop_dance = True
+                self.stop_behaviour = True
                 return STOPPED
 
         try:
