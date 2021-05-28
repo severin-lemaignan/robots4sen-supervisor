3aaa81b9a8f05bc715c30129f0ed6120e47db3c7
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2021-05-28 15:01:42 +0100

    supervisor: wip rewrite to implement a state machine

--- supervisor.py
+++ supervisor.py
@@ -1,6 +1,5 @@
 # utf-8
 
-from events import ActivityEvent
 import logging;logger = logging.getLogger("robots.supervisor")
 
 from csv_logging import create_csv_logger
@@ -15,7 +14,7 @@ from PySide2.QtCore import QUrl, Slot, Signal, QObject, Property
 
 from constants import *
 
-from events import ActivityEvent
+from events import Event
 
 ###########################################
 # ACTIVITIES
@@ -31,7 +30,7 @@ from activities.relax_sounds import activity as relax_sounds
 ###########################################
 
 TICK_PERIOD = 0.05 #sec
-COOL_DOWN_DURATION = 5 #sec
+COOL_DOWN_DURATION = 1 #sec
 
 class Supervisor(QObject):
     def __init__(self, bridge):
@@ -41,8 +40,10 @@ class Supervisor(QObject):
 
         self.bridge = bridge
 
-        self.activity = None
-        self.request_interrupt = False
+        self.activity = default_activity.get_activity()
+        self.activity.start(self.bridge, self.cmd_queue)
+
+        self.nb_engaged = 0 # nb of people the robot is currently engaged with
 
         # rest_time stores the timestamp at which the last activity stopped
         # used to ensure a 'cool down' period between 2 activities
@@ -64,68 +65,78 @@ class Supervisor(QObject):
 
     def run(self):
 
-        while True:
-            self.process_queue()
-            
-            #logger.debug("%s people detected" % len(self.bridge.people.getpeople()))
-            #logger.debug("%s people engaged" % len(self.bridge.people.getengagedpeople()))
+         while True:
+            evt = self.process_events()
 
-            if self.activity:
+            if self.activity.type != DEFAULT \
+               and ((evt.type == Event.INTERRUPTED and evt.src == Event.CTRL_TABLET)
+                    or evt.type == Event.NO_ONE_ENGAGED
+                    or evt.type == Event.NO_INTERACTION):
 
-                if self.activity.type == DEFAULT:
-                    t = time.time() - self.rest_time
-                    if t < COOL_DOWN_DURATION:
-                        logger.warning("Cool-down period (%.1f/%fs)" % (t, COOL_DOWN_DURATION))
-                        continue
+                status = self.activity.tick(evt) # give the chance to close appropriately
+                assert(status == STOPPED)
 
-                    if len(self.bridge.people.getengagedpeople()) > 0:
-                        logger.warning("Someone is engaging! Start moodboard")
-                        self.startActivity(moodboard.get_activity())
+                logger.info("Activity <%s> interrupted (%s)" % (self.activity, evt.type))
+                action_logger.info((self.activity.type, status))
 
-                evt = None
-                if self.activity.type != DEFAULT:
-                    if self.request_interrupt or self.bridge.tablet.isCancellationRequested():
+                self.startActivity(DEFAULT)
 
-                        src = ActivityEvent.CTRL_TABLET if self.request_interrupt else ActivityEvent.PEPPER_TABLET
-                        evt = ActivityEvent(ActivityEvent.INTERRUPTED,src)
-                        action_logger.info((self.activity.type, str(evt)))
+            if self.activity.type == DEFAULT:
 
-                    if len(self.bridge.people.getengagedpeople()) == 0:
-                        evt = ActivityEvent(ActivityEvent.NO_ONE_ENGAGED)
-                        action_logger.info((self.activity.type, str(evt)))
+                t = time.time() - self.rest_time
+                if t < COOL_DOWN_DURATION:
+                    logger.warning("Cool-down period (%.1f/%fs)" % (t, COOL_DOWN_DURATION))
+
+                elif    evt.type == Event.ACTIVITY_REQUEST \
+                     or evt.type == Event.ONE_TO_ONE_ENGAGEMENT \
+                     or evt.type == Event.MULTI_ENGAGEMENT:
+
+                    self.startActivity(MOODBOARD)
+
+            if self.activity.type == MOODBOARD:
+
+                if evt.type == Event.ACTIVITY_REQUEST:
+                    self.startActivity(evt.activity)
 
 
-                status = self.activity.tick(evt)
+            status = self.activity.tick(evt)
 
-                if status == STOPPED:
-                    logger.info("Activity <%s> completed" % self.activity)
-                    action_logger.info((self.activity.type, status))
+            if status == STOPPED:
 
-                    # implement logic to either:
-                    #  - go back to moodboard and either do another activity or ask final mood
-                    #  - or go to rest (default waving hand)
-                    if self.activity == moodboard.get_activity():
-                        self.activity = None
-                        self.request_interrupt = False
-                        self.isCurrentActivity_changed.emit(None)
-                        self.rest_time = time.time()
-                    else:
-                        logger.warning("Returning to moodboard")
-                        self.startActivity(moodboard.get_activity(), True) # continuation = True
+                logger.info("Activity <%s> completed" % self.activity)
+                action_logger.info((self.activity.type, status))
 
-            # if no active activity, and no activity was enqueue, fall back to the
-            # default activity (eg, the waving hand)
-            else:
-                if self.bridge.tablet.isConnected():
-                    logger.info("Starting default activity")
-                    self.activity = default_activity.get_activity()
-                    self.activity.start(self.bridge, self.cmd_queue)
-                else:
-                    pass
-                    #logger.warning("Waiting for Pepper's tablet to be connected")
+                if self.activity.type != MOODBOARD:
+                    # go back to moodboard to ask whether to continue or final mood
+                    self.startActivity(MOODBOARD, True)
+
+                if self.activity.type == MOODBOARD:
+                    # go back to hand waving, and restart cool-down period
+                    self.startActivity(DEFAULT)
+                    self.rest_time = time.time()
+
 
     def startActivity(self, activity, *args):
-        self.activity = activity
+        if activity == DEFAULT:
+            self.activity = default_activity.get_activity()
+        elif activity == MOODBOARD:
+            self.activity = moodboard.get_activity()
+        elif activity == STORY:
+            self.activity = stories.get_activity()
+        elif activity == JOKES:
+            self.activity = jokes.get_activity()
+        elif activity == FUN_DANCES:
+            self.activity = fun_dances.get_activity()
+        elif activity == CALM_MUSIC:
+            self.activity = calm_music.get_activity()
+        elif activity == RELAX_SOUNDS:
+            self.activity = relax_sounds.get_activity()
+        elif activity == CALM_DANCES:
+            self.activity = calm_dances.get_activity()
+        else:
+            logger.error("Unknown activity <%s>" % activity)
+            return
+
         logger.info("Activity <%s> starting" % self.activity)
 
         if args:
@@ -137,47 +148,84 @@ class Supervisor(QObject):
         self.isCurrentActivity_changed.emit(str(self.activity))
 
 
-    def process_queue(self):
+    def process_events(self):
+
+
+        ####################################################################
+        ###
+        ###  ENGAGEMENT/DISENGAGEMENT EVENTS
+
+        nb_currently_engaged = len(self.bridge.people.getengagedpeople())
+        nb_currently_seen = len(self.bridge.people.getpeople())
+
+        # we *only* generate events if:
+        #  - the number of currently engaged person goes down to 0
+        #  - the number of currently engaged person goes _above_ 0
+        #
+        #  We do not generate events if eg going from 1 to 2 person engaged
+
+        if self.nb_engaged != nb_currently_engaged:
+
+            # no one engaged anymore
+            if nb_currently_engaged == 0:
+                self.nb_engaged = nb_currently_engaged
+                return Event(Event.NO_ONE_ENGAGED)
+
+            if self.nb_engaged == 0:
+
+                # only one person around: one-to-one engagement
+                if    nb_currently_engaged == 1 \
+                  and nb_currently_seen == 1:
+                      self.nb_engaged = nb_currently_engaged
+                      return Event(Event.ONE_TO_ONE_ENGAGEMENT)
+
+                # else, several people around the robot. Even if only
+                # one is detected as 'engaged', we trigger a group engagement
+                # event
+                else:
+                      self.nb_engaged = nb_currently_engaged
+                      return Event(Event.MULTI_ENGAGEMENT)
+
+
+        #####################################################################
+        ###
+        ###   CTRL TABLET INTERRUPTION REQUESTS
+
+        if self.bridge.tablet.isCancellationRequested():
+            return Event(Event.INTERRUPTED, src=Event.CTRL_TABLET)
+
+
+        #####################################################################
+        ###
+        ###   ACTIVITY/INTERRUPTION REQUESTS
 
         try:
             source, cmd, args = self.cmd_queue.get(block=True, timeout=TICK_PERIOD)
         except Empty:
-            return
+            return Event()
 
         logger.debug("GOT A %s CMD: %s (%s)" % (source, cmd, args))
         
         if cmd == INTERRUPT:
-            if self.activity:
-                self.request_interrupt = True
-        elif cmd == SOCIAL_GESTURE:
-            self.bridge.animate(args)
-        elif cmd == BEHAVIOUR:
-            self.bridge.run_behaviour(args)
-        elif cmd == LOOK_AT:
-            self.bridge.lookAt(*args)
-        elif cmd == TRACK:
-            if not args:
-                self.bridge.stop_tracking()
-            else:
-                self.bridge.track(args)
+            return Event(Event.INTERRUPTED, src=source)
+        #elif cmd == SOCIAL_GESTURE:
+        #    self.bridge.animate(args)
+        #elif cmd == BEHAVIOUR:
+        #    self.bridge.run_behaviour(args)
+        #elif cmd == LOOK_AT:
+        #    self.bridge.lookAt(*args)
+        #elif cmd == TRACK:
+        #    if not args:
+        #        self.bridge.stop_tracking()
+        #    else:
+        #        self.bridge.track(args)
         elif cmd == MOODBOARD:
-            self.startActivity(moodboard.get_activity())
+            return Event(Event.ACTIVITY_REQUEST, src=source, activity=MOODBOARD)
 
         elif cmd == ACTIVITY:
-                if args == STORY:
-                    self.startActivity(stories.get_activity())
-                elif args == JOKES:
-                    self.startActivity(jokes.get_activity())
-                elif args == FUN_DANCES:
-                    self.startActivity(fun_dances.get_activity())
-                elif args == CALM_MUSIC:
-                    self.startActivity(calm_music.get_activity())
-                elif args == RELAX_SOUNDS:
-                    self.startActivity(relax_sounds.get_activity())
-                elif args == CALM_DANCES:
-                    self.startActivity(calm_dances.get_activity())
-
+            return Event(Event.ACTIVITY_REQUEST, src=source, activity=args)
 
         else:
             logger.error("UNHANDLED CMD FROM %s: %s" % (source, cmd)) 
+
 
