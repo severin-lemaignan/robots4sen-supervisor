381b7d460b7f7a0123026b1abcb9b3b685e178a4
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2021-06-01 10:17:10 +0100

    activity cuddle: port to new class based impl

--- activities/cuddle/activity.py
+++ activities/cuddle/activity.py
@@ -6,33 +6,20 @@ import random
 
 from constants import *
 from dialogues import get_dialogue
-from events import ActivityEvent
+from events import Event
 
-class CalmMusicActivity:
+from activities.activity import Activity
 
-    type = cuddle
+class CuddleActivity(Activity):
+
+    type = CUDDLE
 
     def __init__(self):
-        pass
-
-    def __str__(self):
-        return "cuddle"
-
-    def start(self, robot, cmd_queue):
-
-        self.robot = robot
-        self.cmd_queue = cmd_queue
-        self.response_queue = self.robot.tablet.response_queue
-
-        self.robot.tablet.debug("activity/cuddle")
+        super(CuddleActivity, self).__init__()
 
         self.stop_behaviour = False
 
-        # self._behaviour is a generator returning the current activity status;
-        # self.tick() (called by the supervisor) will progress through it
-        self._behaviour = self.behaviour()
-
-    def behaviour(self):
+    def run(self):
 
         self.robot.tablet.clearOptions()
         self.robot.say(get_dialogue("cuddle_start")).wait()
@@ -48,24 +35,13 @@ class CalmMusicActivity:
                 self.stop_behaviour = False
             yield RUNNING
 
-    def tick(self, evt=None):
+    def terminate(self):
+        self.stop_behaviour = True
+        next(self._behaviour) # run it one more time to ensure the naoqi behaviour is cancelled
+        return STOPPED
 
-        if evt:
-            if evt.type == ActivityEvent.INTERRUPTED:
-                logger.warning("Activity 'cuddle' stopped: interrupt request!");
-                self.stop_behaviour = True
-                return STOPPED
-            if evt.type == ActivityEvent.NO_ONE_ENGAGED:
-                logger.warning("Activity 'cuddle' stopped: no one in front of the robot!");
-                self.stop_behaviour = True
-                return STOPPED
 
-        try:
-            return next(self._behaviour)
-        except StopIteration:
-            return STOPPED
-
-activity = CalmMusicActivity()
+activity = CuddleActivity()
 
 def get_activity():
     return activity
